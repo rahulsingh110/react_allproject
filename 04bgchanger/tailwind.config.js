1b6ba3754c718d0959d740b744d918f30293b0c8
Rahul <rahulsingh01234.ks@gmail.com>
2024-01-19 21:17:58 +0530

    uploded

--- 04bgchanger/tailwind.config.js
+++ 04bgchanger/tailwind.config.js
@@ -1,6 +1,7 @@
- {import('tailwindcss').Config} 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
